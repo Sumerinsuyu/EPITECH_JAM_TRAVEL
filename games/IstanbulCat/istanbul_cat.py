fabc670a7b002ff1b024f60f55dcaa3b376e16f5
sumerinsuyu <paul1.emeriau@epitech.eu>
2025-05-04 02:49:27 +0200

    FEAT: player jump

--- games/IstanbulCat/istanbul_cat.py
+++ games/IstanbulCat/istanbul_cat.py
@@ -2,12 +2,13 @@ import arcade
 
 from arcade.types import Color
 from games.IGame import IGame
-from games.IstanbulCat.player import Player, IDLE, JUMP, RUN
+from games.IstanbulCat.player import Player, JUMP, RUN, FALL
 from arcade.types import LBWH, LRBT, XYWH, Color, Point2List, Rect, RGBOrA255
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-FLOOR = 90
+SCREEN_WIDTH    = 800
+SCREEN_HEIGHT   = 600
+FLOOR           = 90
+BG_SPEED        = -2
 
 def create_rect(x, y, width, height):
     return Rect(x, width, height, y, width, height, width / 2, height / 2)
@@ -17,6 +18,7 @@ class IstanbulCat(IGame, arcade.View):
     def __init__(self):
         super().__init__()
         self.player = Player("WIWIWI", (200, FLOOR))
+        self.player_pos_jump = FLOOR
         self.isOver = False
         self.background_texture = arcade.load_texture("games/IstanbulCat/assets/istanbul_background.png")
         self.background_rect = create_rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -43,26 +45,17 @@ class IstanbulCat(IGame, arcade.View):
     def on_draw(self):
         self.clear()
         self.draw_moving_background()
-
-        arcade.draw_lbwh_rectangle_filled(
-            self.player.position[0],
-            self.player.position[1],
-            self.player.size[0],
-            self.player.size[1],
-            self.player.color
-        )
+        self.update_player()
+        self.draw_player()
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.SPACE:
+        if key == arcade.key.SPACE and self.player.state == RUN:
             self.player.state = JUMP
-            print("JUMPING")
 
     def draw_moving_background(self):
-        # Move both background rectangles left
-        self.background_rect = self.background_rect.move(-2, 0)
-        self.background_rect2 = self.background_rect2.move(-2, 0)
+        self.background_rect = self.background_rect.move(BG_SPEED, 0)
+        self.background_rect2 = self.background_rect2.move(BG_SPEED, 0)
 
-        # If a background has moved completely off screen, reset it to the right
         if self.background_rect.right <= 0:
             self.background_rect = Rect(
             SCREEN_WIDTH,
@@ -87,6 +80,28 @@ class IstanbulCat(IGame, arcade.View):
             SCREEN_HEIGHT / 2
         )
 
-        # Draw both background textures
         arcade.draw_texture_rect(self.background_texture, self.background_rect)
         arcade.draw_texture_rect(self.background_texture, self.background_rect2)
+
+    def draw_player(self):
+        arcade.draw_lbwh_rectangle_filled(
+            self.player.position[0],
+            self.player.position[1],
+            self.player.size[0],
+            self.player.size[1],
+            self.player.color
+        )
+
+    def update_player(self):
+        if self.player.state == JUMP:
+            position = list(self.player.position)
+            position[1] += 2
+            self.player.position = tuple(position)
+            if self.player.position[1] >= FLOOR + 100:
+                self.player.state = FALL
+        if self.player.state == FALL:
+            position = list(self.player.position)
+            position[1] -= 2
+            self.player.position = tuple(position)
+            if self.player.position[1] <= FLOOR:
+                self.player.state = RUN
--- games/IstanbulCat/player.py
+++ games/IstanbulCat/player.py
@@ -1,17 +1,19 @@
 
-IDLE    = 0
-RUN     = 1
-JUMP    = 2
-
 import arcade
 from arcade.types import Color
+
+RUN     = 1
+JUMP    = 2
+FALL    = 3
+
+FLOOR = 90
 
 class Player:
     def __init__(self, name: str, position: tuple = (0.0, 0.0), size: tuple = (50.0, 50.0)):
         self.name = name
         self.position = position
         self.size = size
-        self.state = IDLE
+        self.state = RUN
         self.color = arcade.color.BLUE
 
     def move(self, x: int, y: int):
